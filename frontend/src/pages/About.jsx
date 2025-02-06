0ea59bac0170a01e6ca350b4d16719949a719986
OpenRAG <openrag189@gmail.com>
2025-02-06 10:14:21 +0530

    Update About.jsx
    Photos of advisor , founder and co-founder is visible in mobile view

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -60,40 +60,55 @@ const About = () => {
                 democratize AI and make it accessible worldwide.{" "}
               </p>
             </p>
-            <div className="md:pt-5 hidden md:items-center  md:flex ">
-            <div className=" m-auto text-black font-bold text-lg px-2 w-max flex flex-col gap-2">
-                <div className="md:w-40 md:h-44 lg:w-52 lg:h-56 xl:w-60 xl:h-80 border-4 border-white rounded-xl">
+            <div className="pt-5  md:items-center  flex overflow-y-auto">
+              <div className=" m-auto text-black font-bold text-lg px-2 w-max flex flex-col gap-2">
+                <div className="w-52 h-56 md:w-40 md:h-44 lg:w-52 lg:h-56 xl:w-60 xl:h-80 border-4 border-white rounded-xl">
                   <img
                     src={advisor}
                     alt=""
                     className="w-full h-full object-cover rounded-xl grayscale hover:grayscale-0"
                   />
                 </div>
-                <h1 className="md:text-[.69rem] lg:text-[.79rem] xl:text-sm font-sans">ADVISOR : <br/><span className="text-emerald-300 font-bold"> Suresh Chandra Satpathy</span></h1>
+                <h1 className="text-[.69rem] lg:text-[.79rem] xl:text-sm font-sans">
+                  ADVISOR : <br className="hidden md:block"/>
+                  <span className="text-emerald-300 font-bold">
+                    {" "}
+                    Suresh Chandra Satpathy
+                  </span>
+                </h1>
               </div>
-            
-            <div className=" m-auto text-black font-bold text-lg px-2 w-max flex flex-col gap-2">
-                <div className="md:w-40 md:h-44 lg:w-52 lg:h-56 xl:w-60 xl:h-80 border-4 border-white rounded-xl">
+
+              <div className=" m-auto text-black font-bold text-lg px-2 w-max flex flex-col gap-2">
+                <div className="w-52 h-56 md:w-40 md:h-44 lg:w-52 lg:h-56 xl:w-60 xl:h-80 border-4 border-white rounded-xl">
                   <img
                     src={founder}
                     alt=""
                     className="w-full h-full object-cover rounded-xl grayscale hover:grayscale-0"
                   />
                 </div>
-                <h1 className="md:text-[.69rem] lg:text-[.79rem] xl:text-sm font-sans">FOUNDER : <br/><span className="text-emerald-300 font-bold"> Nisharg Nargund</span></h1>
+                <h1 className="text-[.69rem] lg:text-[.79rem] xl:text-sm font-sans">
+                  FOUNDER : <br className="hidden md:block"/>
+                  <span className="text-emerald-300 font-bold">
+                    {" "}
+                    Nisharg Nargund
+                  </span>
+                </h1>
               </div>
               <div className=" m-auto text-black font-bold text-lg px-2 w-max flex flex-col gap-2">
-                <div className="md:w-40 md:h-44 lg:w-52 lg:h-56 xl:w-60 xl:h-80 border-4 border-white rounded-xl">
+                <div className="w-52 h-56 md:w-40 md:h-44 lg:w-52 lg:h-56 xl:w-60 xl:h-80 border-4 border-white rounded-xl">
                   <img
                     src={cofounder}
                     alt=""
                     className="w-full h-full object-cover rounded-xl grayscale hover:grayscale-0"
                   />
                 </div>
-                <h1 className="md:text-[.69rem] lg:text-[.79rem] xl:text-sm font-sans">CO-FOUNDER : <br/><span className="text-emerald-300 font-bold">Mitul Goswami</span></h1>
+                <h1 className="text-[.69rem] lg:text-[.79rem] xl:text-sm font-sans">
+                  CO-FOUNDER : <br className="hidden md:block"/>
+                  <span className="text-emerald-300 font-bold">
+                    Mitul Goswami
+                  </span>
+                </h1>
               </div>
-
-              
             </div>
           </SpotlightCard>
           <SpotlightCard
@@ -117,8 +132,9 @@ const About = () => {
               </p>
             </p>
           </SpotlightCard>
+          
         </div>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
