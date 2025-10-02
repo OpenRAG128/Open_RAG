1d5e4304fd1878fd9e74c9ab004935519d2287fd
OpenRAG <openrag189@gmail.com>
2025-10-02 16:26:06 +0530

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -15,13 +15,11 @@ const HeroSection = () => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6  lg:px-8 pt-20 pb-16 relative z-10">
             <div className="text-center flex flex-col gap-5">
               <h1 className="text-4xl md:text-6xl xl:text-8xl font-bold text-white mb-6">
-                <span className="block text-[#AD49BD]"> Industry-specialized</span>
-                Generative AI for the Future
+                <span className="block text-[#AD49BD]"> Knowledge Reimagined</span>
+                AI That Thinks in Context, Delivers the Truth.
               </h1>
               <p className="sm:text-[12px] md:text-xl font-sans text-gray-400 max-w-2xl mx-auto mb-8">
-                Experience the future of AI with our domain-specific models,
-                designed to provide precision and efficiency in solving
-                real-world problems across various sectors.
+                Step into the future of AI with our context-powered models, built to deliver unmatched precision and efficiency while solving hallucinations across diverse sectors.
               </p>
               <div className="flex justify-center gap-4">
                 <Link to='/get-in-touch' target="_self" className="px-4 py-1  md:px-8 md:py-3 bg-white text-black rounded-lg font-semibold hover:bg-gray-100 transition-colors">
